1b54a23ddeae7b1769e170771fb10d2c8989365f
Shane Lennon <shane.michael.lennon@gmail.com>
2019-01-12 13:35:18 -0600

    updated server.js

--- server.js
+++ server.js
@@ -35,9 +35,39 @@ app.get("/api/tables", function(req, res) {
   return res.json(tables);
 });
 
+app.get("api/reservations/:reservation", function(req, res) {
+  var table = req.params.reservation;
+
+  console.log(table);
+
+  
+  for (var i = 0; i < tables.length; i++) {
+    if (chosen === tables[i].routeName) {
+      return res.json(tables[i]);
+    }
+  }
+
+  return res.json(false);
+})
+
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+var tables = [
+  {
+    uuid: 1,
+    name: "Joe Blow",
+    phoneNumber: "555-555-5555",
+    email: "joeblow@aol.com"
+  },
+  {
+    uuid: 2,
+    name: "Mary Jane",
+    phoneNumber: "555-420-5555",
+    email: "MJ@aol.com"
+  }
+]
