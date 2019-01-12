b9fccde13a2e2968f2d8b5e11c746a971fcff8b7
redtgrlily <redtgrlily@outlook.com>
2019-01-12 13:33:33 -0600

    revamp reservation table html

--- server.js
+++ server.js
@@ -12,29 +12,77 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Star Wars Characters (DATA)
+// =============================================================
+var reservations = [
+  {
+    Uuid: 1,
+    name: "Yoda",
+    phoneNumber: "8009990909",
+    email: "imamoron@gmail.com"
+  },
+  {
+    Uuid: 2,
+    name: "wah",
+    phoneNumber: "8009990909",
+    email: "imamoron@gmail.com"
+  },
+  {
+    Uuid: 3,
+    name: "okay",
+    phoneNumber: "8009990909",
+    email: "imamoron@gmail.com"
+  }
+];
+
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "view.html"));
 });
-
-app.get("/table", function(req, res) {
-  res.sendFile(path.join(__dirname, "table.html"));
-});
-
 
 app.get("/reservation", function(req, res) {
   res.sendFile(path.join(__dirname, "reservation.html"));
 });
 
-
-// Displays all table JSON data
-app.get("/api/tables", function(req, res) {
-  return res.json(tables);
+// Displays all characters
+app.get("/api/table", function(req, res) {
+  return res.json(characters);
 });
 
+// Displays a single character, or returns false
+app.get("/api/table/:table", function(req, res) {
+  var reserve = req.params.table;
+
+  console.log(reserve);
+
+  for (var i = 0; i < table.length; i++) {
+    if (reserve === table[i].routeName) {
+      return res.json(table[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/table", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newReservation = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  characters.push(newReservation);
+
+  res.json(newReservation);
+});
 
 // Starts the server to begin listening
 // =============================================================
